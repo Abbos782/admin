89d4e019b88bc0e3b91515b277d72158f16eaba0
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-04-01 20:35:58 +0300

    add delet add

--- js/stayl.js
+++ js/stayl.js
@@ -22,43 +22,47 @@ let formElement = document.querySelector(".form");
               resetForm();
             
             
-     
+              
 
   })
  function readFormData(){
    let formData={
-       name:document.getElementById("name").value,
-       surname:document.getElementById("surname").value,
-       email:document.getElementById("email").value,
+       name:document.querySelector("#name").value,
+       surname:document.querySelector("#surname").value,
+       email:document.querySelector("#email").value,
    };
    return formData;
  }
  function insertNewRecord(data){
-     let table = document.getElementById("list").getElementsByTagName("tbody")[0];
+     let table = document.querySelector("#list");
+       table.querySelector("tbody");
+     
      let newRow = table.insertRow(table.length);
-     cell1 = newRow.insertCell(0);
-     cell1.innerHTML = data.name;
-     cell2 = newRow.insertCell(1);
-     cell2.innerHTML = data.surname;
-     cell3 = newRow.insertCell(2);
-     cell3.innerHTML = data.email;
-     cell3 = newRow.insertCell(3);
-     cell3.innerHTML = '<button onClick="editForm(this)"> Edit</button>  <button onclick="onDelete(this)">delet</button>';
-
+     console.log(newRow);
+     row1 = newRow.insertCell(0);
+     row1.innerHTML = data.name;
+     row2 = newRow.insertCell(1);
+     row2.innerHTML = data.surname;
+     row3 = newRow.insertCell(2);
+     row3.innerHTML = data.email;
+     row3 = newRow.insertCell(3);
+     row3.innerHTML = '<button onClick="editForm(this)"> Edit</button>';
+     row4 = newRow.insertCell(4);
+     row4.innerHTML = '<button onclick="onDelete(this)">delet</button>';
 
  }
  function resetForm(){
-  document.getElementById("name").value ="";
-  document.getElementById("surname").value ="";
-  document.getElementById("email").value ="";
+  document.querySelector("#name").value ="";
+  document.querySelector("#surname").value ="";
+  document.querySelector("#email").value ="";
   selectedRow = null;
  }
   
       function editForm(td){
-        selectedRow = td.parentElement.parentElement;
-        document.getElementById("name").value = selectedRow.cells[0].innerHTML;
-        document.getElementById("surname").value = selectedRow.cells[1].innerHTML;
-        document.getElementById("email").value = sleectedRow.cells[2].innerHTML;
+        selectedRow = td.parentElement;
+        document.querySelector("#name").value = selectedRow.cells[0].innerHTML;
+        document.querySelector("#surname").value = selectedRow.cells[1].innerHTML;
+        document.querySelector("#email").value = sleectedRow.cells[2].innerHTML;
 
 
       }
@@ -70,51 +74,9 @@ let formElement = document.querySelector(".form");
      
 function onDelete(td){
   row = td.parentElement.parentElement;
-  document.getElementById("list").deleteRow(row.rowIndex);
+  document.querySelector("#list").deleteRow(row.rowIndex);
     resetForm();
 
 }
 
 
-
-
-
-
-  
-// let videos = [{
-//   src: "https://www.youtube.com/embed/vfN8gscQVqs",
-//   title: "YouTube video player",
-//   allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
-//   allowfullscreen: true
-// }]
-
-// function renderVideos() {
-//   photosList.textContent = ""
-//   videos.forEach(video => {
-//       let iframeElement = createIframe(video.src, video.title, video.allow, video.allowfullscreen)
-//       photosList.appendChild(iframeElement)
-//   })
-// }
-
-// function createIframe(src, title = "Youtube video player", allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", allowfullscreen = true, frameborder = 0) {
-//   let newIframeElement = document.createElement('iframe')
-//   newIframeElement.setAttribute('src', src)
-//   newIframeElement.setAttribute('title', title)
-//   newIframeElement.setAttribute('frameborder', frameborder)
-//   newIframeElement.setAttribute('allow', allow)
-//   newIframeElement.setAttribute('allowfullscreen', allowfullscreen)
-
-//   return newIframeElement
-// }
-
-// formElement.addEventListener('submit', (evt) => {
-//   evt.preventDefault()
-//   videos.push({
-//       src: "https://www.youtube.com/embed/" + videosIdElement.value,
-//       title: videosTitleElement.value,
-//       allow: videosAllowElement.value,
-//       allowfullscreen: videosFullScreenElement.value
-//   })
-//   renderVideos()
-//   evt.target.reset()
-// })
