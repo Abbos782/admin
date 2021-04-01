8ae0bcba7d6d92f2d5772dd9db618b59f3dc3245
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-04-01 13:46:41 +0300

    add

--- js/stayl.js
+++ js/stayl.js
@@ -1,2 +1,34 @@
 let formElement = document.querySelector(".form");
-let formElement = document.querySelector(".form");
+let nameElement = document.querySelector(".name");
+let surnameElement = document.querySelector(".surname");
+let emailElement = document.querySelector(".email");
+let tableElement =document.querySelector('.table');
+//   let userElement =document.querySelector('.user');
+
+let n = 0;
+
+  formElement.addEventListener('submit', e=>{
+      e.preventDefault();
+      n++
+      let trElement = document.createElement('tr');
+         trElement.classList.add("user");
+         console.log(trElement);
+         tableElement.appendChild(trElement);
+         console.log(tableElement);
+      let userElement =document.querySelector('.user');
+      let tdElementn = document.createElement('td');
+      let tdElement1 = document.createElement('td');
+      let tdElement2 = document.createElement('td');
+          tdElementn.document = n;
+         tdElement1.innerHTML = nameElement.value;
+         tdElement2.innerHTML = surnameElement.value;
+         userElement.prepend(tdElementn);
+         userElement.prepend(tdElement1);
+         userElement.prepend(tdElement2);
+         console.log(userElement);
+
+         
+     
+
+  })
+  e.reset();
